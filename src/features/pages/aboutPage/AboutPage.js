4eb43abc690b8d7499ed1d9cc583929f90c74d06
Maria <alece.1414@gmail.com>
2022-07-14 14:13:48 +0300

    aboutPage adaptive added

--- src/features/pages/aboutPage/AboutPage.js
+++ src/features/pages/aboutPage/AboutPage.js
@@ -5,15 +5,13 @@ import './aboutPage.sass';
 const AboutPage = () => {
     return (
         <section className="about">
-            <div className="container">
-                <div className='about__wrapper'>
-                    <img src={src} alt="Alina Khalieva" />
-                    <div>
-                        <h1 className="title about__title">Обо мне</h1>
-                        <p className="about__descr">
-                        Tell us all about your expectations, plans or concerns. We are open for any questions you might have. Let's discuss them face to face or over the phone/email- you pick.
-                        </p>
-                    </div>
+            <div className='about__wrapper'>
+                <img src={src} alt="Alina Khalieva" />
+                <div className="about__info">
+                    <h1 className="title about__title">Обо мне</h1>
+                    <p className="about__descr">
+                    Tell us all about your expectations, plans or concerns. We are open for any questions you might have. Let's discuss them face to face or over the phone/email- you pick.
+                    </p>
                 </div>
             </div>
         </section>
