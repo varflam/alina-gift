2b253f3ddcf14c92e28452ceacb2399caa065684
Maria <alece.1414@gmail.com>
2022-07-14 14:30:43 +0300

    contactPage adaptive added

--- src/features/pages/contactPage/ContactPage.js
+++ src/features/pages/contactPage/ContactPage.js
@@ -9,26 +9,28 @@ const ContactPage = () => {
     return (
         <section className="contact">
             <div className='contact__wrapper'>
-                <h1 className="title contact__title">Мои контакты</h1>
-                <div className="contact__socials__wrapper">
-                    <ul className="contact__socials">
-                        <li className="contact__social">
-                            <img src={email} alt="email" />
-                            <p className="contact__social__descr">info@taraings.ca</p>
-                        </li>
-                        <li className="contact__social">
-                            <img src={insta} alt="insta" />
-                            <p className="contact__social__descr">@taraweddings</p>
-                        </li>
-                        <li className="contact__social">
-                            <img src={phone} alt="phone" />
-                            <p className="contact__social__descr">+1(888)699-9</p>
-                        </li>
-                    </ul>
-                    <p className="contact__descr">
-                    Tell us all about your expectations, plans or concerns. We are open for any questions you might have. Let's discuss them face to face or over the 
-                    phone/email- you pick.
-                    </p>
+                <div className="contact__info">
+                    <h1 className="title contact__title">Мои контакты</h1>
+                    <div className="contact__socials__wrapper">
+                        <ul className="contact__socials">
+                            <li className="contact__social">
+                                <img src={email} alt="email" />
+                                <p className="contact__social__descr">info@taraings.ca</p>
+                            </li>
+                            <li className="contact__social">
+                                <img src={insta} alt="insta" />
+                                <p className="contact__social__descr">@taraweddings</p>
+                            </li>
+                            <li className="contact__social">
+                                <img src={phone} alt="phone" />
+                                <p className="contact__social__descr">+1(888)699-9</p>
+                            </li>
+                        </ul>
+                        <p className="contact__descr">
+                        Tell us all about your expectations, plans or concerns. We are open for any questions you might have. Let's discuss them face to face or over the 
+                        phone/email- you pick.
+                        </p>
+                    </div>
                 </div>
             </div>
             <img src={src} alt="Contact me" />
